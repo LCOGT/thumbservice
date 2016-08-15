bc0502402a6efbb5b7758cd363f23d9f878af878
Edward Gomez <edward@gomez.me.uk>
2016-08-15 16:58:13 -0700

    Removed unnecessary file naming

--- thumbservice.py
+++ thumbservice.py
@@ -25,9 +25,56 @@ def key_for_jpeg(frame_id, **params):
 
 
 def convert_to_jpg(path, key, **params):
-    jpg_path = os.path.dirname(path) + '/' + key
+    if type(path) == list:
+        jpg_path = os.path.dirname(path[0]) + '/' + key
+    else:
+        jpg_path = os.path.dirname(path) + '/' + key
     fits_to_jpg(path, jpg_path, **params)
     return jpg_path
+
+def get_request_from_frame(frame, request):
+    headers = {
+        'Authorization': '8fe46d7e5a37364d955c36c4aed28cfc0719d38b' #request.headers.get('Authorization')
+    }
+    # data = {'username': 'egomez@lcogt.net','password': 'D0ctorWho' }
+    # response = requests.post('https://archive-api.lcogt.net/api-token-auth/',data)
+    # headers = {'Authorization': 'Token ' + response.json()['token']}
+    frames = requests.get(
+        '{0}frames/?REQNUM={1}'.format(ARCHIVE_API, frame['REQNUM']),
+        headers=headers
+    ).json()
+    if frames['count'] == 0:
+        abort(404)
+    return frames['results']
+
+def find_rgb_frames(frame, request):
+    results = get_request_from_frame(frame, request)
+    filters = [r['FILTER'] for r in results]
+    filter_names = set(filters)
+    if len(filter_names) != 3:
+        print('Need 3 filter, {} given.'.format(len(filter_names)))
+        abort(404)
+    else:
+        if 'rp' in filter_names:
+            red_filter = 'rp'
+        elif 'R' in filter_names:
+            red_filter = 'R'
+        else:
+            print('No red filter used')
+            abort(404)
+        filter_dict = {'red':'','B':'','V':''}
+        for f in filter_names:
+            url = [r['url'] for r in results if r['FILTER'] == f]
+            if f == red_filter:
+                filter_dict['red'] = url[0]
+            else:
+                filter_dict[f] = url[0]
+        paths = []
+        for f in ['red','V','B']:
+            data = {'filename':('image_{}.fits.fz').format(f),'url':filter_dict[f]}
+            path = save_temp_file(data)
+            paths.append(path)
+        return paths
 
 
 def upload_to_s3(jpg_path):
@@ -60,27 +107,41 @@ def key_exists(key):
         return False
 
 
-def generate_thumbnail(frame, request):
+def generate_thumbnail(frame, request,args):
     params = {
         'width': int(request.args.get('width', 200)),
         'height': int(request.args.get('height', 200)),
         'label_text': request.args.get('label')
     }
-    key = key_for_jpeg(frame['id'], **params)
+    if request.args.get('color'):
+        image_name = frame['OBJECT']
+    else:
+        image_name = frame['id']
+    key = key_for_jpeg(image_name, **params)
     if key_exists(key):
         return generate_url(key)
     # Cfitsio is a bit crappy and can only read data off disk
-    path = save_temp_file(frame)
+    if request.args.get('color'):
+        path = find_rgb_frames(frame, request)
+        params['color'] = True
+    else:
+        path = save_temp_file(frame)
     jpg_path = convert_to_jpg(path, key, **params)
-    upload_to_s3(jpg_path)
+    # upload_to_s3(jpg_path)
     # Cleanup actions
-    os.remove(path)
-    os.remove(jpg_path)
-    return generate_url(key)
+    if request.args.get('color'):
+        for p in path:
+            os.remove(p)
+    else:
+        os.remove(path)
+    # os.remove(jpg_path)
+    # return generate_url(key)
+    return jpg_path
 
 
 def handle_response(frame, request):
-    url = generate_thumbnail(frame, request)
+    args = request.args.lists()
+    url = generate_thumbnail(frame, request, args)
     if request.args.get('image'):
         r = requests.get(url, stream=True)
         return send_file(
