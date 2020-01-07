7aa9354de3bacead21567e1733520afd4fbdaeab
eheinrich <eheinrichjosties@gmail.com>
2020-01-07 20:44:40 +0000

    Add on_starting hook to clean up any temp files that the other cleanup hook did not get to on a forced pod restart

--- config.py
+++ config.py
@@ -16,3 +16,15 @@ def child_exit(server, worker):
     # If the worker is not killed gracefully, the temp files generated under
     # that worker will need to be cleaned up
     clean_up_files(worker.pid)
+
+
+def on_starting(server):
+    # If the pod is restarted forcefully (for example, for an OOM) then the child exit hook may
+    # even have been run. The on starting hook runs when the master process starts. Clear out
+    # the temp dir if there is anything in there.
+    # https://docs.gunicorn.org/en/stable/settings.html#on-starting
+    paths = glob.glob(f'{settings.TMP_DIR}*')
+    for path in paths:
+        if os.path.exists(path):
+            server.log.info(f'Path {path} was left behind during restart, cleaning it up')
+            os.remove(path)
